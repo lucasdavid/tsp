03d7add348ad17b9ca82115b65eeb9277f780994
lucasdavid <lucasolivdavid@gmail.com>
2013-05-27 19:53:20 -0300

    README.md updated

--- java_nb_project/src/Analyzer/Analyzer.java
+++ java_nb_project/src/Analyzer/Analyzer.java
@@ -8,16 +8,17 @@ import tsp.*;
  */
 public class Analyzer {
 
-    private enum algorithms {
-
-        TWICEAROUND, TWICEAROUNDDIJK, EDGESCORE
-    }
     private final int INTERACTIONS = 10;
     private final int MAX_NODES = 5;
     private final int MIN_NODES = 4;
     private final int MAX_EDGE_VALUE = 10;
     private final int MIN_EDGE_VALUE = 1;
     private final String GRAPH_FILE = "bayg29.tsp";
+    private enum algorithms {
+
+        TWICEAROUND, TWICEAROUNDDIJK, EDGESCORE
+    }
+    
     private Tsp currentProblem;
     private double[] algorithmsCosts;
     private algorithms[] algorithmsCompared;
